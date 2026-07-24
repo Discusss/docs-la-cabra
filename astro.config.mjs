e3a29d203f84acec51f593889c848c1ef13d6feb
ThePhoDit <42234092+ThePhoDit@users.noreply.github.com>
2026-07-24 16:44:00 +0200

    Project structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,57 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import { escapeHTML } from 'astro/runtime/server/escape.js';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: {
+				en: 'LaCabra Docs',
+				es: 'Documentación LaCabra',
+			},
+			defaultLocale: 'es',
+			locales: {
+				// English docs in `src/content/docs/en/`
+				en: {
+					label: 'English',
+				},
+				// Simplified Chinese docs in `src/content/docs/zh-cn/`
+				es: {
+					label: 'Español',
+					lang: 'es',
+				},
+			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
+				{ 
+					label: "Introduction",
+					slug: "intro/intro",
+					translations:{
+						en: "Introduction",
+						es: "Introducción"
+					}
+				},
 				{
-					label: 'Guides',
+					label: 'Commands',
+					translations: {
+						en: 'Commands',
+						es: 'Comandos'
+					},
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide', slug: 'commands/example' },
 					],
 				},
 				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					label: 'Configuration',
+					translations: {
+						en: 'Configuration',
+						es: 'Configuración'
+					},
+					items: [
+						{ slug: 'config/howto' },
+					],
 				},
 			],
 		}),
---
+++ todos.md
@@ -0,0 +1,26 @@
+- [x] Introducción
+- [ ] Donaciones
+- [ ] Badges
+- [ ] Estado
+- [ ] Tutoriales
+- [ ] Comandos
+- [ ] Configuración Intro
+- [ ] Configuración Autorizaciones
+- [ ] Configuración Censor
+- [ ] Configuración Comandos Omitidos
+- [ ] Configuración Logs
+- [ ] Configuración Moderación
+- [ ] Configuración Prefijo
+- [ ] Configuración Spam
+
+
+## Cosas nuevas
+
+- [ ] Moderación AntiBots (anuncios)
+- [ ] Apelaciones y apelaciones múltiples (anuncios)
+- [ ] Módulo anti-spam y funcionalidad anti-raid (anuncios testers, la config está más arriba)
+- [ ] Canales de voz temporales (anuncios testers)
+- [ ] Tiempos compuestos en las duraciones (anuncios testers)
+- [ ] Comandos mmodinf, parche, pato, zorro (anuncios testers)
+- [ ] Mensajes personalizados por MD y sus variables (anuncios testers)
+- [ ] Mutes nativos de Discord
